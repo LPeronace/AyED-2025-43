cb32de9803f068b1b7045957ddaa7f409c80a52e
LPeronace <lautaro.peronace@gmail.com>
2025-05-12 09:16:30 -0300

    Add files via upload
    Subiendo

---
+++ Etapa0/0_Peronace_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etapa_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hola Mundo";
+
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
