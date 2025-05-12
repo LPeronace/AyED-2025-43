d9708c801c070d2756c13a2315542673c88a56d9
LPeronace <lautaro.peronace@gmail.com>
2025-05-12 09:24:09 -0300

    Add files via upload
    Termine de subir e 0

---
+++ 0_Peronace_12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Decime tres letras:");
+            string letras = Console.ReadLine();
+
+
+            Console.WriteLine("tus letras son:" + letras + " y el invertido de esas 3 letras es:" + letras[2] + letras[1] + letras[0]);
+            Console.ReadKey();
+
+        }
+    }
+}
