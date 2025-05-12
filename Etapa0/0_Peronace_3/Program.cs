c02927a231ed285b6fef832d616fbaefd1f23658
LPeronace <lautaro.peronace@gmail.com>
2025-05-12 09:18:50 -0300

    Add files via upload
    Subiendo

---
+++ Etapa0/0_Peronace_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etapa_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Decime tu nombre:");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Decime tu edad:");
+            string edad = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + " tu edad es:" + edad);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa0/0_Peronace_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etapa_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+
+            Console.WriteLine("Decime un Numero");
+            a = Console.ReadLine();
+            int n1 = int.Parse(a);
+
+            Console.WriteLine("Decime otro");
+            a = Console.ReadLine();
+            int n2 = int.Parse(a);
+
+            int suma = n1 + n2;
+            Console.WriteLine("La suma de los dos numeros es:" + suma);
+
+            Console.ReadKey();
+        }
+    }
+}
