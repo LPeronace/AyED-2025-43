acaaa2d82402d06a5da6396131ad5dea9a50b2e2
LPeronace <lautaro.peronace@gmail.com>
2025-05-15 09:26:42 -0300

    Add files via upload

---
+++ Etapa1/1_Peronace_10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Peronace_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            do
+            {
+                if (numero % 3 == 0 && numero % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (numero % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (numero % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(numero);
+                }
+                numero++;
+
+            } while (numero <= 100);
+            Console.ReadKey();
+        }
+    }
+}
