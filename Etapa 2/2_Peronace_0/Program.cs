e5bf41b67b0e59cc14d7a5dcd36f01dd65b76672
LPeronace <lautaro.peronace@gmail.com>
2025-05-19 09:42:25 -0300

    Add files via upload
    subida tp0 y 1

---
+++ Etapa 2/2_Peronace_0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Peronace_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            Console.WriteLine("Cuantos invitados tenes?");
+            int[] invitados = new int[int.Parse(Console.ReadLine())];
+            int[] comida = new int[invitados.Count()];
+
+            for (int i = 0; i < invitados.Count(); i++)
+            {
+                Console.WriteLine("Y Cuanta comida come el invitado "+ (i+1)+"? (de 1 a 100)");
+                comida[i] = int.Parse(Console.ReadLine());
+
+                total += comida[i];
+                double promedio = total / invitados.Count();
+                Console.WriteLine("El total es:" + total + " y el promedio es:"+promedio); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/2_Peronace_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Peronace_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cuantas vueltas hiciste?:");
+            int vueltas = int.Parse(Console.ReadLine());
+            int[] tiempo = new int[vueltas];
+
+            int tiempototal = 0, vueltarapida=9999999;
+
+            for(int i = 0; i < vueltas; i++)
+            {
+                Console.Write("Tiempo de vuelta?:");
+                tiempo[i] = int.Parse(Console.ReadLine());
+                tiempototal += tiempo[i];
+                
+                if(vueltarapida > tiempo[i])
+                {
+                    vueltarapida = tiempo[i];
+                }
+            }
+            double promedio = tiempototal / vueltas;
+
+            Console.WriteLine("El tiempo total fue:" + tiempototal+ " segundos, el promedio:"+promedio +" segundos y la vuelta mas rapida:"+vueltarapida +" segundos");
+
+
+            Console.ReadKey();
+        }
+    }
+}
