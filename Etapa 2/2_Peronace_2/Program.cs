1d8e4f135cb981e236f7f6a6bd74eadef09f3a65
LPeronace <lautaro.peronace@gmail.com>
2025-05-20 20:46:21 -0300

    Add files via upload
    Me olvide de subirlos

---
+++ Etapa 2/2_Peronace_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Peronace_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totaltp = 0, totalexamen = 0;
+
+            Console.WriteLine("Cuantos tps tenes?");
+            int[] cantidadtp = new int[int.Parse(Console.ReadLine())];
+            int[] notatp = new int[cantidadtp.Count()];
+
+            Console.WriteLine("Cuantos pruebas tenes?");
+            int[] cantidadexamen = new int[int.Parse(Console.ReadLine())];
+            int[] notaexamen = new int[cantidadexamen.Count()];
+
+            for (int i = 0; i < cantidadtp.Count(); i++)
+            {
+                Console.WriteLine("Nota del tp "+ (i+1)+" ?");
+                notatp[i] = int.Parse(Console.ReadLine());
+                totaltp += notatp[i];
+            }
+            for (int i = 0; i < cantidadexamen.Count(); i++)
+            {
+                Console.WriteLine("Nota de la prueba " + (i + 1) + " ?");
+                notaexamen[i] = int.Parse(Console.ReadLine());
+                totalexamen += notaexamen[i];
+            }
+            double promedioxamen = totalexamen / cantidadexamen.Count();
+            double porcentaje = (75 / totaltp) * 100;
+
+            if (promedioxamen >= 6 && porcentaje >= 6) 
+            {
+                Console.WriteLine("Aprobaron!!!");
+            }
+            else
+            {
+                Console.WriteLine("Desaprobaron :(");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/2_Peronace_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Peronace_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos participantes hay?");
+            int[] personas =new int[int.Parse(Console.ReadLine())];
+            int[] puntaje = new int[personas.Count()];
+            int[] ordenado = new int[personas.Count()];
+            for (int i = 0; i < personas.Count(); i++)
+            {
+                Console.WriteLine("Cuanto puntaje hizo el participante numero " + (i + 1) + " ?");
+                puntaje[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int a = 0; a < personas.Count(); a++)
+            {
+                int residuo = 0;
+                if()
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
