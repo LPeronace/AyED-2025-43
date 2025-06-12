5fe1d848f13eb1cdc137d9399a46070828ec81bf
LPeronace <lautaro.peronace@gmail.com>
2025-06-12 09:26:52 -0300

    Subida tp 12 y 13

---
+++ Etapa 2/2_Peronace_12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Peronace_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decime cuanto tamaño tiene la matriz");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[n,n];
+            int[] vector = new int[n];
+            Random aleatorio = new Random();
+            int v = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j=0; j < n; j++)
+                {
+                    matriz[i, j] = aleatorio.Next(1, 100);
+                    Console.Write(matriz[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = matriz[i, v];
+                v++;
+                Console.WriteLine(vector[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/2_Peronace_13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Peronace_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos alumnos tenes?");
+            int alumno = int.Parse(Console.ReadLine());
+            string[,] matriz = new string[alumno, 3];
+
+            int j = 0;
+
+            for(int i = 0; i < alumno; i++)
+            {
+                Console.Write("Decime el nombre del alumno "+(i+1)+":");
+                matriz[i, j] = Console.ReadLine();
+
+                Console.Write("Cuantos años tiene el alumno" + (i + 1) + "?:");
+                matriz[i, j + 1] = (int.Parse(Console.ReadLine())).ToString();
+
+                Console.Write("Y que nota tiene el alumno" + (i + 1) + "?:");
+                matriz[i, j + 2] = (int.Parse(Console.ReadLine())).ToString();
+
+            }
+            for(int i = 0; i < alumno; i++)
+            {
+                for(int v = 0; v < alumno; v++)
+                {
+                    Console.Write(matriz[i, v]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
