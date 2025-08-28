bbf8e52aab4af574ae5928db5149673e3ea7122c
LPeronace <lautaro.peronace@gmail.com>
2025-08-28 18:32:29 -0300

    subida una parte

---
+++ Etapa 3/3_Peronace_4/3_Peronace_4_WD1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nivel 1 – Validación de llave (LITE)");
+        bool ok = Level1.ValidateAccessKey("WD-700000")
+                  && !Level1.ValidateAccessKey("WD-123123")
+                  && !Level1.ValidateAccessKey("WX-000007")
+                  && !Level1.ValidateAccessKey("WD-00007");
+        if (ok) Console.WriteLine("✔ UNLOCK → Fragmento: CT");
+        else Console.WriteLine("🔒 LOCKED");
+        Console.ReadKey();
+    }
+}
+
+static class Level1
+{
+    // Debe devolver true solo si:
+    // - Empieza por "WD-"
+    // - Luego hay exactamente 6 dígitos
+    // - La suma de esos 6 dígitos es múltiplo de 7
+    public static bool ValidateAccessKey(string key)
+    {
+        bool a = false;
+        int b = 0,uno,dos,tres,cuatro,cinco,seis;
+        if (key.StartsWith("WD-") && key.Length == 9)
+        {
+            if (char.IsDigit(key[3]) && char.IsDigit(key[4]) && char.IsDigit(key[5]) && char.IsDigit(key[6]) && char.IsDigit(key[7]) && char.IsDigit(key[8]))
+            {
+                uno = key[3] - '0';
+                dos = key[4] - '0';
+                tres = key[5] - '0';
+                cuatro = key[6] - '0';
+                cinco = key[7] - '0';
+                seis = key[8] - '0';
+                b = (uno + dos + tres + cuatro + cinco + seis) % 7;
+                if(b == 0)
+                {
+                    a = true;
+                }
+            }
+        }
+        return a;
+        // TODO: implementar
+        // <- reemplazar por tu solución
+    }
+}
---
+++ Etapa 3/3_Peronace_4/3_Peronace_4_WD3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nivel 3 – Firewalls adyacentes (LITE)");
+        int[,] g =
+        {
+            {0,1,0},
+            {1,0,1},
+            {0,1,0}
+        };
+        bool ok = Level3.CountAdjacent(g, 1, 1) == 4
+               && Level3.CountAdjacent(g, 0, 0) == 2;
+        Console.WriteLine(ok ? "✔ UNLOCK → Fragmento: -OK" : "🔒 LOCKED");
+        Console.ReadKey();
+    }
+}
+
+static class Level3
+{
+    public static int CountAdjacent(int[,] grid, int row, int col)
+    {
+        // TODO: implementar
+        int vecinos = 0;
+        for(int i = 0; i <= grid.GetLength(0); i++)
+        {
+            for(int j = 0; j <= grid.GetLength(1); j++)
+            {
+                if(!(row-1< 0) && !(col-1 < 0) && !(row+1 > grid.GetLength(0)) && !(col+1 > grid.GetLength(1)))
+                {
+                    if(grid[row-1,col] == 1)
+                    {
+                        vecinos += 1;
+                    }
+                    else if (grid[row + 1, col] == 1)
+                    {
+                        vecinos += 1;
+                    }
+                    else if (grid[row, col-1] == 1)
+                    {
+                        vecinos += 1;
+                    }
+                    else if (grid[row, col+1] == 1)
+                    {
+                        vecinos += 1;
+                    }
+                } 
+            }
+        }
+
+        // Considerar vecinos: (r-1,c), (r+1,c), (r,c-1), (r,c+1)
+        // Devolver cuántos valen 1
+        return vecinos; // <- reemplazar por tu solución
+    }
+}
